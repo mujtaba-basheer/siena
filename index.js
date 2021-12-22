e27648dc3c964c112b1725c8bb20f920158b6ce9
Faiz Bachoo Shah <faizbshah2001@gmail.com>
2021-12-22 17:46:18 +0530

    Implemented Cart Functions

--- index.js
+++ index.js
@@ -32,6 +32,51 @@ const formFields = [
   },
 ];
 
+// Adding an item to cart
+const addItemToCart = (newItem) => {
+  const cart = getCart();
+
+  if (!cart) {
+    return;
+  }
+
+  const itemIndex = cart.findIndex(item => item._id === newItem._id);
+
+  itemIndex !== -1 ? 
+    (cart[itemIndex] = newItem) : 
+    (cart.push(newItem));
+
+  setCart(cart);
+}
+
+// Removing an item from cart
+const removeItemFromCart = (removedItem) => {
+  const cart = getCart();
+
+  if (!cart) {
+    return;
+  }
+
+  const newCart = cart.filter(item => item._id !== removedItem._id);
+
+  setCart(newCart);
+}
+
+// Getting the cart from localStorage
+const getCart = () => {
+  return JSON.parse(localStorage.getItem("cart") || null);
+}
+
+// Setting new cart in localStorage
+const setCart = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Get no. of items in cart
+const getCartSize = () => {
+  return getCart().length;
+}
+
 const isFormValid = () => {
   let flag = true;
 
